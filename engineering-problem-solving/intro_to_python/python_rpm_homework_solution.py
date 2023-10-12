8d2100f3a8d1bfac1f1d99660e3c5caaf6966406
Zak Oster <zc9oster@me.com>
2023-10-12 10:12:27 -0500

    Changes to intro to python.

--- engineering-problem-solving/intro_to_python/python_rpm_homework_solution.py
+++ engineering-problem-solving/intro_to_python/python_rpm_homework_solution.py
@@ -3,13 +3,13 @@ import math
 import statistics
 from matplotlib import pyplot
 
-tire_diameter = 18
+tire_diameter = int(input("Tire Diameter: "))
 tire_circumference = tire_diameter * math.pi
 inches_to_miles = 1 / 63360
 min_per_hour = 60
 conversion_factor = tire_circumference * inches_to_miles * min_per_hour
 
-with open("tire_rpm_homework.csv", "r") as file_contents:
+with open("tire_rpm_homework.csv", "r", encoding="utf-8") as file_contents:
     csv_reader = csv.reader(file_contents, delimiter=",")
     for rpm_list in csv_reader:
         speed = [conversion_factor * int(x) for x in rpm_list]
@@ -20,13 +20,13 @@ speed_mean = statistics.mean(speed)
 speed_median = statistics.median(speed)
 speed_mode = statistics.mode(speed)
 
-print("The max speed was {:.0f}mph.".format(speed_max))
-print("The min speed was {:.0f}mph.".format(speed_min))
-print("The mean speed was {:.0f}mph.".format(speed_mean))
-print("The median speed was {:.0f}mph.".format(speed_median))
-print("The mode speed was {:.0f}mph.".format(speed_mode))
+print(f"The max speed was {speed_max:.0f}mph.")
+print(f"The min speed was {speed_min:.0f}mph.")
+print(f"The mean speed was {speed_mean:.0f}mph.")
+print(f"The median speed was {speed_median:.0f}mph.")
+print(f"The mode speed was {speed_mode:.0f}mph.")
 
-time = [i for i in range(0, len(speed))]
+time = range(0, len(speed))
 pyplot.plot(time, speed, label="Vehicle Speed")
 pyplot.title("Vehicle Speed at a Given Time")
 pyplot.xlabel("Time")
@@ -52,5 +52,5 @@ pyplot.show()
 # Numerically
 for count, mph in enumerate(speed):
     if mph >= 60:
-        print("Time from 0-60mph: {:.2f}s".format(count/300))
+        print(f"Time from 0-60mph: {(count/300):.2f}s")
         break
